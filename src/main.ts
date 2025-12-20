f509e23740afb7878d5d0f06fad3ac6861fc8595
habib33-3 <habib.rahman0330@gmail.com>
2025-12-20 09:45:26 +0600

    basic swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,35 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as packageJson from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle(packageJson.name)
+    .setDescription(packageJson.description)
+    .setVersion(packageJson.version)
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('doc', app, documentFactory, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   // Add global API versioning prefix
   app.setGlobalPrefix('api/v1');
 
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
+  console.log(
+    `Application is running on: http://localhost:${process.env.PORT ?? 3000}`,
+  );
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Error during application bootstrap:', err);
+});
