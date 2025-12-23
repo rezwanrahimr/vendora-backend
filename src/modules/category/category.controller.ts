e6f3e170f318ecdc0e2f987df94397a02ee734cb
habib33-3 <habib.rahman0330@gmail.com>
2025-12-23 14:36:20 +0600

    category flow

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -3,24 +3,65 @@ import {
   Get,
   Post,
   Body,
-  Patch,
+  UseInterceptors,
+  UploadedFile,
   Param,
-  Delete,
+  Patch,
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/category.dto';
+import { ApiConsumes } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  categoryImageStorage,
+  imageFileFilter,
+} from 'src/common/utils/file-upload.utils';
+import { FileSizeInterceptor } from 'src/common/interceptors/file-size.interceptor';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoryService.createCategory(createCategoryDto);
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: categoryImageStorage,
+      fileFilter: imageFileFilter,
+    }),
+    FileSizeInterceptor,
+  )
+  create(
+    @Body() dto: CreateCategoryDto,
+    @UploadedFile() file?: Express.Multer.File,
+  ) {
+    return this.categoryService.createCategory(dto, file);
   }
 
   @Get()
   findAll() {
     return this.categoryService.getAllCategories();
+  }
+
+  @Get('/vendors/:id')
+  findVendorsByCategory(@Param('id') id: string) {
+    return this.categoryService.getVendorsByCategory(id);
+  }
+
+  @Patch(':id')
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: categoryImageStorage,
+      fileFilter: imageFileFilter,
+    }),
+    FileSizeInterceptor,
+  )
+  update(
+    @Param('id') id: string,
+    @Body() dto: CreateCategoryDto,
+    @UploadedFile() file?: Express.Multer.File,
+  ) {
+    return this.categoryService.updateCategory(id, dto, file);
   }
 }
